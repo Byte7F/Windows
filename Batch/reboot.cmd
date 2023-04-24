1bcfc9a4e0c72f6262cdbb289c65a5fcb6c514c6
0x7F <105129250+Byte7F@users.noreply.github.com>
2023-04-24 14:35:10 -0600

    Update reboot.cmd

--- Batch/reboot.cmd
+++ Batch/reboot.cmd
@@ -1,3 +1,10 @@
+:: Script usage:
+::               installation: %windir%\System32\reboot.cmd
+::                application: Command Prompt with Administrator privs; quick-access: [Win]+[X] -> [A]dmin Command Prompt
+::
+:: Command usage:
+::                reboot [<*>.exe|<name>]
+::
 @SetLocal
 @set args=%*
 @call kill.cmd %args%
