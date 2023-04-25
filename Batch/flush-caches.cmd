e2191a8490d115d44dc173222d0553fc85c35ad7
0x7F <105129250+Byte7F@users.noreply.github.com>
2023-04-25 07:10:55 -0600

    Update flush-caches.cmd

--- Batch/flush-caches.cmd
+++ Batch/flush-caches.cmd
@@ -29,7 +29,7 @@
 :help_
 @echo.
 @echo flush-caches
-@echo              --user [username] :: sets profile for data to be cleared of, uses current username otherwise   &REM use_userprofile;use_user
+@echo              --user [username] :: sets profile for data to be cleared of, uses current username otherwise   &REM use_user;use_userprofile
 @echo              --use-event [0^|1] :: opens "Event Viewer" if you feel generous to manually scan ^& clear logs &REM use_eventvwr
 @echo              --use-procs [0^|1] :: opens an array of built-in Cleaners: DiskCleanup, WinStoreReset, DeFrag  &REM use_procs
 @echo              --kill-auto [0^|1] :: keeps "Windows Update: Auto-Update (Service)" ^(wuauserv^) stopped       &REM end_wuauserv
